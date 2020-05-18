cd2a60eef5e077cfffb893b2943b414d799a3cbf
NotQuiteConfident <38504392+NotQuiteConfident@users.noreply.github.com>
2020-05-18 15:27:32 -0400

    Final Project (complete)

--- Final_Project_SLR/Final_Project_SLR.R
+++ Final_Project_SLR/Final_Project_SLR.R
@@ -6,21 +6,24 @@ library(Hmisc)
 
 #Get Data
 setwd("C:/Users/hayde/Documents/GitHub/INST408F")
-data <- sasxport.get("Final_Project_SLR/BRFSS18.xpt")
+data <- read.xport("Final_Project_SLR/BRFSS18.xpt")
 
 # Recode drvisits
-data$drvisits[data$drvisits == 88 ] <- 0
-data$drvisits[data$drvisits > 76 ] <- NA
+data$DRVISITS[data$DRVISITS == 88 ] <- 0
+data$DRVISITS[data$DRVISITS > 76 ] <- NA
 
 # Recode poorhlth
-data$poorhlth[data$poorhlth == 88 ] <- 0
-data$poorhlth[data$poorhlth > 30 ] <- NA
+data$POORHLTH[data$POORHLTH == 88 ] <- 0
+data$POORHLTH[data$POORHLTH > 30 ] <- NA
 
 # Simple Linear Regression
-slr <- lm(data = data, poorhlth ~ drvisits)
+slr <- lm(data = data, POORHLTH ~ DRVISITS)
 summary(slr)
 
 # Plot Data in a scatter plot
-ggplot(data=data,aes(x=drvisits, y=poorhlth))+
+ggplot(data=data,aes(x=DRVISITS, y=POORHLTH))+
   labs(title="Days in the last 30 days in Poor Health by Doctor Visits in the last year",x="Doctor Visits", y = "Days in Poor Health")+
   geom_point(shape=1)
+
+par(mfrow = c(2, 2))
+plot(slr)
